eee23b6bfd900a63d43d1ff86fc309af86f91b3e
lizhagey <lizgrhagey@gmail.com>
2016-09-24 15:17:38 -0700

    Add home Page

---
+++ web/cs313/assignments.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Home | CS313</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="style.css" type="text/css" rel="stylesheet" media="screen"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta name="description" content="Home page for CS313">
+    </head>
+    <body>
+        <nav>
+          <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="assignments.php">Assignments</a></li>
+          </ul>  
+        </nav>
+        <div id="body_background">
+        <h1>Assignments</h1>
+        </div>
+        
+    </body>
+</html>
+
