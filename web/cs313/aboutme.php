f85110371c92dee92b500eb796e9899f5f5caed7
lizhagey <lizgrhagey@gmail.com>
2016-09-24 17:59:23 -0700

    adding about me page

---
+++ web/cs313/aboutme.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+        <title>About Me | CS313</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="style.css" type="text/css" rel="stylesheet" media="screen"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta name="description" content="About me page for CS313">
+    </head>
+    <body>
+        <nav>
+          <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="assignments.php">Assignments</a></li>
+            <li><a href="aboutme.php">About Me</a></li>
+          </ul> 
+        </nav>
+        <div id="body_background">
+        <div id='one'>
+                <div class="flip-container">
+                        <div class="flipper" onclick="this.classList.toggle('flipped')">
+                                <div class="front">
+                                    <img src="images/photo.jpg" alt="Photo" width="380px">
+                                    <p id="bottom">Click to see more</p>
+                                </div>
+                                <div id="backone" class="back">
+                                    <h3 id='flip_heading'>About Me</h3>
+                                        <ul id='flip_list'>
+                                            <li>Senior at BYUI</li>
+                                            <li>6 Kids ages 28 to 19</li>
+                                            <li>Live in Las Vegas</li>
+                                            <li>Born in Tennessee</li>
+                                            <li>Majoring in Web Design & Development</li>
+                                        </ul>
+                                        <p id='flip_paragraph'>I love that I have learned so much in college. I have
+                                        gone from zero knowledge about programming to where I am 
+                                        now. I still have so much to learn and I am overwhelmed 
+                                        with all of the work involved but I know that the sacrifice
+                                        will be worth it in the end.</p>
+                                        
+                                </div>
+                        </div>
+                </div>
+             </div>
+ 
+
+        </div>
+    </body>
+</html>
+
