99841f1565e69cbde4ef7d3f341fa7302accc397
lizhagey <lizgrhagey@gmail.com>
2016-09-29 20:00:32 -0700

    fixing errors

--- web/cs313/assignments.php
+++ web/cs313/assignments.php
@@ -1,11 +1,19 @@
 <html>
     <head>
-        <title>Home | CS313</title>
-        <?php include 'modules/head.php'; ?>
+        <title>Assignments | CS313</title>
+         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="style.css" type="text/css" rel="stylesheet" media="screen"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+        <meta name="description" content="Home page for CS313">
     </head>
     <body>
         <nav>
-            <?php include 'modules/nav.php'; ?>
+          <ul>
+              <li><a href="home.php">Home</a></li>
+              <li><a href="assignments.php">Assignments</a></li>
+              <li><a href="aboutme.php">About Me</a></li>
+          </ul>  
         </nav>
         <div id="body_background">
         <h1>Assignments</h1>
